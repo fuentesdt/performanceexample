ac76a5ab0d62e19c4e3763d9d7caf803ad64aa8e
David Fuentes <fuentesdt@gmail.com>
2023-08-26 17:43:42 -0500

    ENH: example output

--- driver.m
+++ driver.m
@@ -5,6 +5,10 @@ load('extraParams.mat' )
 for iii = 1:7
 disp(sprintf('run %d',iii))
 tic
+   profile on
 [initConst.ineq,initConst.ceq, initConst.ineqGrad,initConst.ceqGrad] = reducedConstraint(Xfull ,extraParamscon );
+   myprof = profile('info')
+   save(sprintf('profiledata%d',iii)'','myprof')
+   profile off
 toc
 end
